a912b01d055d968cb9c2a1ba89ec327da95315c2
xkc <xkc@xkc.cn>
2016-03-03 22:23:26 +0800

    求最长回文子串，参考讨论区的代码

--- src/medium/Solution_5.java
+++ src/medium/Solution_5.java
@@ -8,75 +8,76 @@ import java.util.ResourceBundle;
  */
 public class Solution_5 {
 
-//    /**
-//     * 方法1:暴力法
-//     * @param s
-//     * @return
-//     */
-//    public static String longestPalindrome(String s) {
-//        String ans = "";
-//        int length = s.length();
-//        int maxPalindromeLength = 0;
-//        //通过循环得出s的所有子串，求出长度最长的回文子串，暴力方法超时
-//        for (int i = 0; i < length; i++) {
-//            for (int j = i + 1; j < length; j++) {
-//                int subLen = j - i;
-//                String sub = s.substring(i, j + 1);
-//                if (isPalindromeString(sub)) {
-//                    if (subLen > maxPalindromeLength) {
-//                        ans = sub;
-//                        maxPalindromeLength = subLen;
-//                    }
-//                }
-//            }
-//        }
-//        return ans;
-//    }
-//
-//    /**
-//     * 判断s是否是回文字符串
-//     *
-//     * @param s
-//     * @return
-//     */
-//    public static boolean isPalindromeString(String s) {
-//        for (int i = 0; i < s.length(); i++) {
-//            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
-//                return false;
-//            }
-//        }
-//
-//        return true;
-//    }
-
-
     /**
-     * 方法2:动态规划
+     * 方法1:暴力法超时
+     *
      * @param s
      * @return
      */
     public static String longestPalindrome(String s) {
-        if (s == null){
-            return null;
-        }
-        if (s.length() == 1){
-            return s;
-        }
-
-        int maxLen = 1;
-        String longStr = null;
+        String ans = "";
         int length = s.length();
-        int[][] table = new int[length][length];
+        int maxPalindromeLength = 0;
+        //通过循环得出s的所有子串，求出长度最长的回文子串，暴力方法超时
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j < length; j++) {
+                int subLen = j - i;
+                String sub = s.substring(i, j + 1);
+                if (isPalindromeString(sub)) {
+                    if (subLen > maxPalindromeLength) {
+                        ans = sub;
+                        maxPalindromeLength = subLen;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
 
-        for (int i = 0; i < length; i ++){
-            table[i][i] = 1;
+    /**
+     * 判断s是否是回文字符串
+     *
+     * @param s
+     * @return
+     */
+    public static boolean isPalindromeString(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+                return false;
+            }
         }
 
-        return longStr;
-
+        return true;
     }
 
 
-
+    /**
+     * modified by xkc on 2/26/16
+     */
+    public static String method(String s) {
+        String ans = "";
+        int currentLength = 0;
+        for (int i = 0; i < s.length(); i ++){
+            if (isPalindrome(s,i - currentLength - 1, i)){
+                ans = s.substring(i - currentLength - 1, i + 1);
+                currentLength = currentLength + 2;
+            }else if (isPalindrome(s, i - currentLength, i)){
+                ans = s.substring(i - currentLength, i + 1);
+                currentLength = currentLength + 1;
+            }
+        }
+        return ans;
+    }
+    public static boolean isPalindrome(String s, int begin, int end){
+        if (begin < 0){
+            return false;
+        }
+        while (begin < end){
+            if (s.charAt(begin ++) != s.charAt(end --)){
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
