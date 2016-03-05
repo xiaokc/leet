db0b886c09bcb9fc6bdf103c7bc03caacb9bae01
xkc <xkc@xkc.cn>
2016-03-05 20:06:05 +0800

    判断一个整数是否是3的幂

---
+++ src/easy/Solution_326.java
@@ -0,0 +1,19 @@
+package easy;
+
+/**
+ * power of three
+ * 判断是否是3的幂
+ * Created by xkc on 3/3/16.
+ */
+public class Solution_326 {
+    public static void main(String[] args){
+        boolean b = isPowerOfThree(31);
+        System.out.println(b);
+    }
+    public static boolean isPowerOfThree(int n) {
+       // 1162261467 is 3^19,  3^20 is bigger than int
+
+        return  n >= 1 && 1162261467 % n == 0;
+
+    }
+}
